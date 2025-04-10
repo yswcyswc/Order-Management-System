c0f3938b1fa1e55353cea9074aff9dbf9fb1d30a
Cathy Wang <cathyw@andrew.cmu.edu>
2025-04-10 15:49:53 -0400

    new

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,67 +1,67 @@
-class OrdersController < ApplicationController
-  before_action :check_login
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
-  authorize_resource
+# class OrdersController < ApplicationController
+#   before_action :check_login
+#   before_action :set_order, only: [:show, :edit, :update, :destroy]
+#   authorize_resource
 
-  include Cart
+#   include Cart
 
-  def index
-    @all_orders = Order.chronological.to_a
-  end
+#   def index
+#     @all_orders = Order.chronological.to_a
+#   end
 
-  def show
-    @order_items = @order.order_items.includes(:item)
-    @other_orders = @order.customer.orders.where.not(id: @order.id).chronological.to_a
-  end
+#   def show
+#     @order_items = @order.order_items.includes(:item)
+#     @other_orders = @order.customer.orders.where.not(id: @order.id).chronological.to_a
+#   end
 
-  def new
-    @order = Order.new
-  end
+#   def new
+#     @order = Order.new
+#   end
 
-  def create
-    @order = Order.new(order_params)
-    @order.date = Date.current
+#   def create
+#     @order = Order.new(order_params)
+#     @order.date = Date.current
 
-    if @order.save
-      save_each_item_in_cart(@order)
-      clear_cart
-      flash[:notice] = "Thank you for ordering from Roi du Pain."
-      redirect_to order_path(@order)
-    else
-      render :new
-    end
-  end
+#     if @order.save
+#       save_each_item_in_cart(@order)
+#       clear_cart
+#       flash[:notice] = "Thank you for ordering from Roi du Pain."
+#       redirect_to order_path(@order)
+#     else
+#       render :new
+#     end
+#   end
 
-  def edit
-  end
+#   def edit
+#   end
 
-  def update
-    if @order.update(order_params)
-      flash[:notice] = "Order was revised in the system."
-      redirect_to order_path(@order)
-    else
-      render :edit
-    end
-  end
+#   def update
+#     if @order.update(order_params)
+#       flash[:notice] = "Order was revised in the system."
+#       redirect_to order_path(@order)
+#     else
+#       render :edit
+#     end
+#   end
 
-  def destroy
-    if @order.order_items.shipped.empty?
-      @order.destroy
-      flash[:notice] = "Order was removed from the system."
-      redirect_to orders_path
-    else
-      flash[:error] = "Order cannot be removed from the system because some items have shipped."
-      redirect_to order_path(@order)
-    end
-  end
+#   def destroy
+#     if @order.order_items.shipped.empty?
+#       @order.destroy
+#       flash[:notice] = "Order was removed from the system."
+#       redirect_to orders_path
+#     else
+#       flash[:error] = "Order cannot be removed from the system because some items have shipped."
+#       redirect_to order_path(@order)
+#     end
+#   end
 
-  private
+#   private
 
-  def set_order
-    @order = Order.find(params[:id])
-  end
+#   def set_order
+#     @order = Order.find(params[:id])
+#   end
 
-  def order_params
-    params.require(:order).permit(:customer_id, :address_id, :grand_total, :expiration_month, :expiration_year, :credit_card_number)
-  end
-end
+#   def order_params
+#     params.require(:order).permit(:customer_id, :address_id, :grand_total, :expiration_month, :expiration_year, :credit_card_number)
+#   end
+# end
